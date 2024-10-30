cfedc4ac496ec6c2c453e4dbb132445b9335330d
Bhomesh <bhomeshrazdan@gmail.com>
2024-10-30 17:40:55 +0530

    updated tf

--- main.tf
+++ main.tf
@@ -4,17 +4,6 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
-variable "allowed_ip" {
-  description = "IP address allowed to access the restricted role"
-  type        = string
-  default     = "15.207.116.54"  # Replace with your actual IP address
-}
-
-variable "aws_region" {
-  description = "AWS region to deploy resources"
-  type        = string
-  default     = "ap-south-1"
-}
 
 resource "aws_iam_policy" "ip_restricted" {
   name        = "ip-restricted-policy"
@@ -148,6 +137,3 @@ resource "aws_iam_user_policy_attachment" "user_assume_unrestricted" {
   policy_arn = aws_iam_policy.assume_unrestricted_role.arn
 }
 
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "your-unique-bucket-name"  # Replace with a unique bucket name
-} 
