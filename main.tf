62443d6cde0e215384c0184ffd22771d89302048
Bhomesh <bhomeshrazdan@gmail.com>
2024-10-30 17:38:39 +0530

    updated tf

--- main.tf
+++ main.tf
@@ -4,73 +4,18 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
-# Create IAM users
-resource "aws_iam_user" "user" {
-  name = "user"
+variable "allowed_ip" {
+  description = "IP address allowed to access the restricted role"
+  type        = string
+  default     = "15.207.116.54"  # Replace with your actual IP address
 }
 
-# Attach unrestricted role policy to users
-resource "aws_iam_user_policy_attachment" "user_assume_unrestricted" {
-  user       = aws_iam_user.user.name
-  policy_arn = aws_iam_policy.assume_unrestricted_role.arn
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-south-1"
 }
 
-# Restricted Role
-resource "aws_iam_role" "restricted_role" {
-  name = "restricted-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          AWS = aws_iam_role.unrestricted_role.arn
-        }
-      }
-    ]
-  })
-}
-
-# Unrestricted Role
-resource "aws_iam_role" "unrestricted_role" {
-  name = "unrestricted-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        }
-      }
-    ]
-  })
-}
-
-# Attach IP restriction policy to restricted role
-resource "aws_iam_role_policy_attachment" "restricted_role_ip" {
-  role       = aws_iam_role.restricted_role.name
-  policy_arn = aws_iam_policy.ip_restricted.arn
-}
-
-# Allow unrestricted role to assume restricted role
-resource "aws_iam_role_policy_attachment" "unrestricted_role_assume" {
-  role       = aws_iam_role.unrestricted_role.name
-  policy_arn = aws_iam_policy.assume_restricted_role.arn
-}
-
-# Attach S3 full access policy to restricted role
-resource "aws_iam_role_policy_attachment" "restricted_role_s3" {
-  role       = aws_iam_role.restricted_role.name
-  policy_arn = aws_iam_policy.s3_full_access.arn
-} 
-
-
-# IP Restricted Policy
 resource "aws_iam_policy" "ip_restricted" {
   name        = "ip-restricted-policy"
   description = "Policy that restricts access to specific IP"
@@ -93,7 +38,22 @@ resource "aws_iam_policy" "ip_restricted" {
   })
 }
 
-# Assume Restricted Role Policy
+resource "aws_iam_policy" "s3_full_access" {
+  name        = "s3-full-access-policy"
+  description = "Policy providing full access to S3"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "s3:*"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_policy" "assume_restricted_role" {
   name        = "assume-restricted-role-policy"
   description = "Policy allowing unrestricted role to assume restricted role"
@@ -112,7 +72,6 @@ resource "aws_iam_policy" "assume_restricted_role" {
   })
 }
 
-# Assume Unrestricted Role Policy
 resource "aws_iam_policy" "assume_unrestricted_role" {
   name        = "assume-unrestricted-role-policy"
   description = "Policy allowing user to assume unrestricted role"
@@ -131,19 +90,64 @@ resource "aws_iam_policy" "assume_unrestricted_role" {
   })
 }
 
-# S3 Full Access Policy
-resource "aws_iam_policy" "s3_full_access" {
-  name        = "s3-full-access-policy"
-  description = "Policy providing full access to S3"
+resource "aws_iam_role" "restricted_role" {
+  name = "restricted-role"
 
-  policy = jsonencode({
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
+        Action = "sts:AssumeRole"
         Effect = "Allow"
-        Action = "s3:*"
-        Resource = "*"
+        Principal = {
+          AWS = aws_iam_role.unrestricted_role.arn
+        }
       }
     ]
   })
+}
+
+resource "aws_iam_role" "unrestricted_role" {
+  name = "unrestricted-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "restricted_role_ip" {
+  role       = aws_iam_role.restricted_role.name
+  policy_arn = aws_iam_policy.ip_restricted.arn
+}
+
+resource "aws_iam_role_policy_attachment" "restricted_role_s3" {
+  role       = aws_iam_role.restricted_role.name
+  policy_arn = aws_iam_policy.s3_full_access.arn
+}
+
+resource "aws_iam_role_policy_attachment" "unrestricted_role_assume" {
+  role       = aws_iam_role.unrestricted_role.name
+  policy_arn = aws_iam_policy.assume_restricted_role.arn
+}
+
+resource "aws_iam_user" "user" {
+  name = "user"
+}
+
+resource "aws_iam_user_policy_attachment" "user_assume_unrestricted" {
+  user       = aws_iam_user.user.name
+  policy_arn = aws_iam_policy.assume_unrestricted_role.arn
+}
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "your-unique-bucket-name"  # Replace with a unique bucket name
 } 
