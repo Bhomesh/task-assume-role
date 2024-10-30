d33211320b81659c5dd6de5e39a29e2230078141
Bhomesh <bhomeshrazdan@gmail.com>
2024-10-30 17:58:46 +0530

    updated tf

--- main.tf
+++ main.tf
@@ -113,10 +113,10 @@ resource "aws_iam_role" "unrestricted_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "restricted_role_ip" {
-  role       = aws_iam_role.restricted_role.name
-  policy_arn = aws_iam_policy.ip_restricted.arn
-}
+# resource "aws_iam_role_policy_attachment" "restricted_role_ip" {
+#   role       = aws_iam_role.restricted_role.name
+#   policy_arn = aws_iam_policy.ip_restricted.arn
+# }
 
 resource "aws_iam_role_policy_attachment" "restricted_role_s3" {
   role       = aws_iam_role.restricted_role.name
